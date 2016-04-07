08a01a68324a11aca59535c6c393eb35c8259679
= <darthkrallt@gmail.com>
2016-04-07 08:02:58 -0700

    Made build process for background script for JPM.
    Since JPM doesn't allow you to have multiple background scripts (must use like require.js), it is easier to just concatenate the common background script with the JPM index script on build.

--- build-firefox-jpm.sh
+++ build-firefox-jpm.sh
@@ -8,12 +8,16 @@ cp -r data build/
 cp -r lib build/
 cp -r src build/
 
-# Remove files we don't want in the build
-rm build/background.js  # WebExtensions main script
-
 cp package.json build/package.json
 cp LICENSE.txt build/
 cp README.md build/
+
+# Remove files we don't want in the build
+rm build/background-chrome.js  # WebExtensions main script
+
+# Build the background script
+cp index-jpm-firefox.js-raw build/index-jpm-firefox.js
+cat src/background.js >> build/index-jpm-firefox.js
 
 # Build the firefox add on
 cd build; jpm xpi; cp ao3rdr.xpi ../ao3rdr-jpm.xpi
