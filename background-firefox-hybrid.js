2f098cdbd7d49645110c2045ae321d8fa1f5d66d
= <darthkrallt@gmail.com>
2017-10-10 23:32:19 -0700

    FF hybrid

--- background-firefox-hybrid.js
+++ background-firefox-hybrid.js
@@ -1,10 +1,14 @@
 const webext = require("sdk/webextension");
 const {setSyncLegacyDataPort} = require("./src/storage-firefox-hybrid");
 
+console.log("inside hybrid launcher");
+
 webext.startup().then(({browser}) => {
   browser.runtime.onConnect.addListener(port => {
     if (port.name === "sync-legacy-addon-data") {
-      setSyncLegacyDataPort(port);
+        console.log("start sync");
+        setSyncLegacyDataPort(port);
+        console.log("synched");
     }
   });
 });
